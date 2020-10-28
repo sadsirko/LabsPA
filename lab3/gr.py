16086931f136b05529b8b1cdad2ac416ce7f334a
sadsirko <den673883@gmail.com>
2020-10-29 01:30:52 +0200

    added files

--- lab3/gr.py
+++ lab3/gr.py
@@ -1,21 +1,23 @@
 import random
 import networkx as nx
 from matplotlib import pyplot as plt
-#def create_graph()
 
-def print_hi(num_of_nodes):
 
-    chance = 0.3
+def create_gr(num_of_nodes,chance):
+
     G = nx.gnp_random_graph(num_of_nodes, chance, 1, False)
-    nx.draw_circular(G, node_color='red', node_size=100, with_labels=True)
+    nx.draw_random(G, node_color='red', node_size=100, with_labels=True)
     plt.show()
     return G
     #plt.savefig("Graph.png", format="PNG")
 
 
-def print_edges(G):
+def avg_edges(G):
+    sum = 0
     cnt = 0
     for j in nx.to_dict_of_dicts(G):
         for i in nx.to_dict_of_dicts(G)[cnt]:
-            print(cnt, '-', i)
+            #print(cnt, '-', i)
+            sum += 1
         cnt += 1
+    print('avg_eges', sum / cnt)
--- lab3/main.py
+++ lab3/main.py
@@ -4,14 +4,15 @@ import gr
 import cl_gen
 import random
 
-mutation = 0.4#need to complete
-kliks = []    #
+mutation = 70  # need to complete (in percents)*
 num = 10
-iteration = 300
-sons = 4
-G = gr.print_hi(num)
-
-arr = cl_gen.Generation(num, G)
+iteration = 5000
+sons = 15
+#chance_of_conn = 0.05#for 300
+#chance_of_conn = 0.15#for 100
+chance_of_conn = 0.3
+G = gr.create_gr(num, chance_of_conn)
+arr = cl_gen.Generation(num, G, mutation)
 arr.first_gen_cr()
 
 
@@ -19,25 +20,29 @@ arr.first_gen_cr()
 
 
 def main(iter, array):
-    def take_rand_parents(array):
-        rn1 = random.randint(0, len(array.population) - 1)
-        rn2 = random.randint(0, len(array.population) - 1)
+    def take_rand_parents(arrayN):
+        rn1 = random.randint(0, len(arrayN.population) - 1)
+        rn2 = random.randint(0, len(arrayN.population) - 1)
         # prevent dublicating
         while rn1 == rn2:
-            rn2 = random.randint(0, len(array.population) - 1)
+            rn2 = random.randint(0, len(arrayN.population) - 1)
         return rn1, rn2
 
     i = 0
     while i < iter:
         # find two random parents
-        mother, father = take_rand_parents(arr)
-        arr.crossover(mother, father, sons)
+        mother, father = take_rand_parents(array)
+        array.crossover(mother, father, sons)
         i += 1
-
+    #array.show_full_pop()
+    array.find_kliks()
+    print('pop: ', len(array.population))
 
 main(iteration, arr)
-loc = num
-for i in arr.population:
-    if loc < 0:
-        print(i.gen)
-    loc -= 1
+
+for i in arr.kliks:
+    print(i)
+gr.avg_edges(G)
+arr.population[0].first_gen_update(0,G)
+for i in nx.find_cliques(G):
+   print('asd', i)
